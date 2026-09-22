74015b369df6b6b117de1b665740693ab68b43a7
Joel Stransky <stranskydesign@gmail.com>
2026-09-21 21:52:43 -0700

    fix(ui): embed official logo into in-app HTML views and add build.rs rerun triggers

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 #[cfg(windows)]
 fn main() {
+    println!("cargo:rerun-if-changed=src/assets/icon.ico");
+    println!("cargo:rerun-if-changed=build.rs");
     let mut res = winres::WindowsResource::new();
     res.set_icon("src/assets/icon.ico");
     if let Err(e) = res.compile() {
